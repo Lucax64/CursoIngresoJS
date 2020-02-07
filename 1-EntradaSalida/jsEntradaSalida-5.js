561d7ddf598b3da76c48b7d4e63b1a1fa53b0368
Lucax64 <60760334+Lucax64@users.noreply.github.com>
2020-02-07 20:42:04 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,12 +2,16 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
+    //Declaro las variable
     var nombre;
     var edad;
 
+//Tomos los datos desde las cajas de texto
+//"(elNombre y laEdad salen del html)"
     nombre = document.getElementById("elNombre").value;
     edad = document.getElementById("laEdad").value;
 
+//+ para concatenar (unir) entre los strings
     alert("Usted se llama " + nombre + " y tiene " + edad + " años");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,31 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var num1
+var num2
+var resultado
 
+//De texto a numero
+num1 = parseInt(document.getElementById("numeroUno").value);
+num2 = parseInt(document.getElementById("numeroDos").value);
+
+/*Otra manera:
+
+num1 = (document.getElementById("numeroUno").value);
+num2 = (document.getElementById("numeroDos").value);
+
+num1 = parseInt(num1);
+num2 = parseInt(num2); 
+*/
+
+resultado = num1 + num2;
+
+alert("La suma es " + resultado);
 }
 
+/*
+STRING + STRING = STRING
+STRING + NUMBER = STRING
+NUMBER + STRING = STRING
+NUMBER + NUMBER = NUMBER
+*/
