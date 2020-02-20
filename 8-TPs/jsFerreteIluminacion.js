b7fab33992a5b06adc7073d7cfcf360c8f767de5
Lucax64 <60760334+Lucax64@users.noreply.github.com>
2020-02-20 14:48:20 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -8,11 +8,37 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
+function CalcularPrecio() {
      var lamparas = parseInt(document.getElementById("Cantidad").value);
-     var precio;
+     var marca = document.getElementById("Marca").value;
+     var precio 
+     
+     precio = lamparas * 35;
 
-     
-     
+     if (lamparas >= 6) {
+          document.getElementById("precioDescuento").value = precio - (precio * 0.5);
+     }
+
+     else if (lamparas = 5) {
+          switch (marca) {
+               case "ArgentinaLuz":
+                    document.getElementById("precioDescuento").value = precio - (precio * 0.4);
+                    break;
+               default:
+                    document.getElementById("precioDescuento").value = precio - (precio * 0.3);
+          }
+     }
+
+     else if (lamparas = 4) {
+          switch (marca) {
+               case "ArgentinaLuz":
+               case "FelipeLamparas":
+                    document.getElementById("precioDescuento").value = precio - (precio * 0.25);
+                    break;
+               default:
+                    document.getElementById("precioDescuento").value = precio - (precio * 0.2);
+          }
+     }
+
+
 }
