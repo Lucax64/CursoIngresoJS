b8434b9921c1af0fa59d79ec582323b0edbc55ff
Lucax64 <60760334+Lucax64@users.noreply.github.com>
2020-02-27 22:17:06 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,23 @@
 function mostrar()
 {
+    var numero;
+    var contadorDivisores = 0;
 
+    numero = parseInt(prompt("Ingrese un numero"));
 
+    while (isNaN(numero)){
+        numero = parseInt(prompt("Eso no es numero. Ingrese un numero: "));
+    }
+
+    for (i = 1; i <= numero; i++) {
+
+        if (numero % i == 0) {
+            console.log(i);
+            contadorDivisores++;
+        }
+    }
+
+    console.log("Cantidad de divisores encontrados: " + contadorDivisores);
 
 
 }//FIN DE LA FUNCIÓN
